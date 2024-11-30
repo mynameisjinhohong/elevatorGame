6e3feec5cd7c2b18401feb5302851fe1cc451d03
MarbinSpectrum <9507ym@naver.com>
2024-11-30 14:40:22 +0900

    add special ch

--- Assets/YMK/CharacterObj/SpeicalObj/SpeicalCharacterObj.cs
+++ Assets/YMK/CharacterObj/SpeicalObj/SpeicalCharacterObj.cs
@@ -8,6 +8,19 @@ using Sirenix.OdinInspector;
 public class SpeicalCharacterObj : CharacterObj
 {
     [SerializeField] private Image character;
+    [SerializeField] private Sprite normalFace;
+    [SerializeField] private Sprite angryFace;
 
-    public Sprite GetImg() => character.sprite;
+    public Sprite GetImg() => normalFace;
+
+    public Sprite GetAngrySprite() => angryFace;
+
+    protected override void SetAngry(bool state)
+    {
+        //state : 화난 여부
+        if (state)
+            character.sprite = angryFace;
+        else
+            character.sprite = normalFace;
+    }
 }
