14b9b76822835388396e817ba233af91ce5a3772
MarbinSpectrum <9507ym@naver.com>
2024-11-30 22:39:53 +0900

    floordata

---
+++ Assets/HJH/Script/FloorData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloorData", menuName = "Scriptable Objects/FloorData")]
+public class FloorData : ScriptableObject
+{
+    public CharacterData[] characterList;
+}
+
+
--- Assets/HJH/Script/StageData.cs
+++ Assets/HJH/Script/StageData.cs
@@ -1,16 +1,7 @@
-using NUnit.Framework;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "StageData", menuName = "Scriptable Objects/StageData")]
 public class StageData : ScriptableObject
 {
-    public FloorData[] floorData;
+    public FloorData[] floorDatas;
 }
-
-[CreateAssetMenu(fileName = "FloorData", menuName = "Scriptable Objects/FloorData")]
-public class FloorData : ScriptableObject
-{
-    public CharacterData[] characterList;
-}
-
-
